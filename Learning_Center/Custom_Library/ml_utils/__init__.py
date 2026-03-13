680af1cd6161cf896edde8be1bc67b987a270122
Vikram Vadhirajan <viva.1205@gmail.com>
2026-03-13 11:28:38 +0530

    Updated The custom Library init calling

--- Learning_Center/Custom_Library/ml_utils/__init__.py
+++ Learning_Center/Custom_Library/ml_utils/__init__.py
@@ -1,2 +1,19 @@
 #init__.py is a special file in Python that is used to indicate that the directory it is present in is a package. 
 # It can be an empty file or it can contain initialization code for the package. When you import a package, the code in __init__.py is executed, allowing you to set up any necessary variables, functions, or classes that should be available when the package is imported.
+
+# from . import data_cleaning
+# from . import feature_engineering
+# from . import outliers
+# from . import scalar
+
+# __all__ = [
+#     "data_cleaning",
+#     "feature_engineering",
+#     "outliers",
+#     "scalar"
+# ]
+
+from .data_cleaning import *
+from .feature_engineering import *
+from .outliers import *
+from .scalar import *
